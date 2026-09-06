7da9d1b7a078a9f194cad19fbe95c413aa0b8202
Surya Sharma <suryasharma73077@gmail.com>
2026-09-06 12:56:56 +0530

    Time: 5 ms (60.59%), Space: 19 MB (53.17%) - LeetHub

---
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        int a=0;
+        int n=s.length();
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'){
+                a++;
+            }
+        }
+        if(a==0){
+            return false;
+        }
+        return true;
+    }
+};
